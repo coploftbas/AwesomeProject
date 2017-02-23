1a6cbf0e6942fde6a955d61ad6e446b62357a59e
Pipat Waitayaworanart <coploftbas@gmail.com>
2017-02-23 11:22:03 +0900

    Remove Pickers
    remove pickers and rollback to navigate.

--- index.ios.js
+++ index.ios.js
@@ -39,32 +39,34 @@ export default class AwesomeProject extends Component {
     }
     return (
 
-      
-      //<MyScene />
-      <Navigator
-        initialRoute = {{ title: 'My Initial Scene', index: 0 }}
-        renderScene = {(route, navigator) => {
-          return <MyScene
-                   title = {route.title}
 
-                   //Function to call when a new scene should be displayed
-                   onForward = {() => {
-                      const nextIndex = route.index + 1;
-                      navigator.push({
-                        title: 'Scene ' + nextIndex,
-                        index: nextIndex,
-                      });
-                    }}
 
-                   //Function to call to go back to the previous scene
-                   onBack = {() => {
-                      if( route.index > 0) {
-                        navigator.pop();
-                      }
-                    }}
-                  />
-        }}
-      />
+        <Navigator
+          initialRoute = {{ title: 'My Initial Scene', index: 0 }}
+          renderScene = {(route, navigator) => {
+            return <MyScene
+                     title = {route.title}
+
+                     //Function to call when a new scene should be displayed
+                     onForward = {() => {
+                        const nextIndex = route.index + 1;
+                        navigator.push({
+                          title: 'Scene ' + nextIndex,
+                          index: nextIndex,
+                        });
+                      }}
+
+                     //Function to call to go back to the previous scene
+                     onBack = {() => {
+                        if( route.index > 0) {
+                          navigator.pop();
+                        }
+                      }}
+                    />
+          }}
+        />
+
+
 
 
 
